bd9232d43e3d035f2d8ff872d4ac65a2a6a73ee7
Nonoka22 <szekelytundenoemi@gmail.com>
2020-06-16 12:48:44 +0300

    Created Notification functionality

--- app/src/main/java/com/example/myfitnessbuddy/events/NotificationEvent.java
+++ app/src/main/java/com/example/myfitnessbuddy/events/NotificationEvent.java
@@ -1,20 +1,30 @@
 package com.example.myfitnessbuddy.events;
 
-import com.example.myfitnessbuddy.models.Notifications;
+import com.example.myfitnessbuddy.models.NotificationData;
 
 public class NotificationEvent {
 
-    private Notifications notification;
+    private NotificationData notification;
+    private int position;
 
-    public NotificationEvent(Notifications notification) {
+    public NotificationEvent(NotificationData notification, int position) {
         this.notification = notification;
+        this.position = position;
     }
 
-    public Notifications getNotification() {
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public NotificationData getNotification() {
         return notification;
     }
 
-    public void setNotification(Notifications notification) {
+    public void setNotification(NotificationData notification) {
         this.notification = notification;
     }
 }
